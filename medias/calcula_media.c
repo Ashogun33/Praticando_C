2d0063e8dbe4fc1979adf2ef10a350a058daf3a3
Vandilson <ashogun33@gmail.com>
2024-11-21 15:13:05 -0300

    add_(3/5)notas() substituido por float calcularMedia(int quantidade), falta incluir float validaarNota(){}

--- medias/calcula_media.c
+++ medias/calcula_media.c
@@ -2,33 +2,27 @@
 #include <stdlib.h>
 
 void menu(void);
-float add_3notas();
-float add_5notas(); 
-
-float soma, media;
-
+float calculaMedia(int quantidade);
+float validarNota();
 
 void menu(void) {
-
   int opcao = 0;
 
-  printf("1. Calcular média com 3 notas.\n");
-  printf("2. Calcular média com 5 notas.\n");
-  printf("3. Sair\n");
-  
   do {
-  
-    printf("\nEscolha sua opção: ");
-  
+
+    printf("\n1. Calcular média com 3 notas.\n");
+    printf("2. Calcular média com 5 notas.\n");
+    printf("3. Sair\n");
+    printf("Escolha sua opção: ");
     scanf("%d", &opcao);
 
     switch(opcao) {
       case 1:
-      add_3notas();
+      calculaMedia(3);
       break;
 
       case 2:
-      add_5notas();
+      calculaMedia(5);
       break;
 
       case 3:
@@ -41,96 +35,15 @@ void menu(void) {
   } while (opcao!=1 && opcao!=2 && opcao!=3);
 }
 
-  float add_3notas() {
+  float calculaMedia(int quantidade) {
     
-  float nota3[3];
+    float soma = 0;
 
-    printf("Adicione a primeira nota: ");
-    scanf("%f", &nota3[0]);
-
-    while (nota3[0] < 0 || nota3[0] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota3[0]);
+    for (int i =0; i < quantidade; ++i) {
+      printf("Adicione a nota %d: ", i + 1);
+      soma += validarNota();
     }
-
-    printf("\nAdicione a segunda nota: ");
-    scanf("%f", &nota3[1]);
-
-    while (nota3[1] < 0 || nota3[1] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota3[1]);
-    }
-
-    printf("\nAdicione a terceira nota: ");
-    scanf("%f", &nota3[2]);
-
-    while (nota3[2] < 0 || nota3[2] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota3[2]);
-    }
-    
-    int j;
-
-    for (j = 0; j < 3; ++j) {
-        soma += nota3[j]; 
-    }
-
-    return media = soma / 3;
-  }
-  
-
-  float add_5notas() {
-    
-    float nota5[5];
-    
-    printf("Adicione a primeira nota: ");
-    scanf("%f", &nota5[0]);
-
-    while (nota5[0] < 0 || nota5[0] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota5[0]);
-    }
-
-    printf("\nAdicione a segunda nota: ");
-    scanf("%f", &nota5[1]);
-
-    while (nota5[1] < 0 || nota5[1] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota5[1]);
-    }
-
-    printf("\nAdicione a terceira nota: ");
-    scanf("%f", &nota5[2]);
-
-    while (nota5[2] < 0 || nota5[2] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota5[2]);
-    }
-    
-    printf("\nAdicione a quarta nota: ");
-    scanf("%f", &nota5[3]);
-
-    while (nota5[3] < 0 || nota5[3] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota5[3]);
-    }
-
-    printf("\nAdicione a quinta nota: ");
-    scanf("%f", &nota5[4]);
-
-    while (nota5[4] < 0 || nota5[4] > 10) {
-      printf("\nAdicione uma nota entre 0 e 10: ");
-      scanf("%f", &nota5[4]);
-    }
-    
-    int i;
-
-    for (i = 0; i < 5; ++i) {
-        soma += nota5[i]; 
-    }
-
-    return media = soma / 5;
-    
+    return soma / quantidade;
   }
 
 int main() {
